b1a3304dc2850f72ff7c032390e576bfb0d52fa2
kawax <kawaxbiz@gmail.com>
2021-07-01 22:06:18 +0900

    Only local

--- src/SailBackupServiceProvider.php
+++ src/SailBackupServiceProvider.php
@@ -16,7 +16,7 @@ class SailBackupServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
+        if ($this->app->runningInConsole() && $this->app->isLocal()) {
             $this->commands([
                 SailMySQLBackup::class,
             ]);
